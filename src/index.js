86cd4b45c8f7b085b0a4ec1bffe3417e678ef4b9
aytac78 <93247080+aytac78@users.noreply.github.com>
2025-08-09 10:12:54 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import BusinessApp from "./BusinessApp";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<BusinessApp />);
