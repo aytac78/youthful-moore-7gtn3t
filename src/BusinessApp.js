60c41df4b3c9ec60a2a5c844dcfb542322f6b333
aytac78 <93247080+aytac78@users.noreply.github.com>
2025-08-14 11:42:23 +0300

    Update BusinessApp.js

---
+++ src/BusinessApp.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function BusinessApp() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "3rem" }}>
+      <h1>Business Panel Başladı ✅</h1>
+      <p>Bu sayfa artık düzgün şekilde çalışıyor.</p>
+    </div>
+  );
+}
